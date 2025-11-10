8c80bdc45260216ba2ce57d87f6e2e6427b2a0d6
Stefan Mijic <arabojacob170@gmail.com>
2025-11-10 23:58:32 +0100

    add layout for public and private side of the app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './layouts/public-layout/public-layout';
+import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+import { LandingPage } from './pages/public/landing-page/landing-page';
+import { DashboardHome } from './pages/dashboard/dashboard-home/dashboard-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: PublicLayout,
+        children: [
+            { path: '', component: LandingPage },
+        ]
+    },
+    {
+        path: 'app',
+        component: DashboardLayout,
+        children: [
+            { path: '', component: DashboardHome },
+        ]
+    }
+];
